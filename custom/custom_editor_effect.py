ec2a0f92749e4a68b2ca13a685744a70bea87b87
kylo <huiboxes@gmail.com>
2023-07-18 21:01:25 +0800

    feat: 添加飞行井道具

--- custom/custom_editor_effect.py
+++ custom/custom_editor_effect.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134242904: {'model_id': 134242904, 'name': u'blade', 'describe': 134242904, 'key': 134242904, 'res_identify': u'', 'scale': 1.0, 'uid': 134242904}}
--- custom/custom_model.py
+++ custom/custom_model.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134242904: {'name': u'blade', 'model_id': 134242904, 'meshes': [], 'effect_path': 'Fx_Effect/Realism/Fx_Skill/Knife/674f7c5e-0210-4a8d-a9c3-6b6844e3ecf6', 'custom': True}}
